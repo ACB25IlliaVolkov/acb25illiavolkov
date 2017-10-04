03c3ab151a1befb510fabb94271c2144b1507f5a
ilya.volkov <illiamvolkov@gmail.com>
2017-10-04 20:06:37 +0300

    add

---
+++ src/CodingBat/Arrayhas22.java
@@ -0,0 +1,30 @@
+package CodingBat;
+
+import myClass.AllOfMe;
+
+public class Arrayhas22 {
+
+    public static void main(String[] args) {
+
+
+        int[] nums = AllOfMe.createMassWithRandomNumber(10, 6, 25);
+
+        int numsLength = nums.length;
+
+        boolean rez = false;
+
+        for (int i = 0; i < numsLength; i++) {
+
+            if (nums[i] == 2 && ((i == (numsLength - 1)) ? false : nums[i + 1] == 2)) {
+                rez = true;
+            }
+
+        }
+
+        AllOfMe.printMass(nums);
+
+        System.out.println(rez);
+
+    }
+}
+
