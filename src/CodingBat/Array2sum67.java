5a906db8bd4a7a38ebcfb09e76fa4e8d9c4d67e6
ilya.volkov <illiamvolkov@gmail.com>
2017-10-04 20:06:20 +0300

    add

---
+++ src/CodingBat/Array2sum67.java
@@ -0,0 +1,39 @@
+package CodingBat;
+
+import myClass.AllOfMe;
+
+public class Array2sum67 {
+    public static void main(String[] args) {
+
+
+        int[] nums = AllOfMe.createMassWithRandomNumber(5, -15, 9);
+
+        int sum = 0;
+
+        boolean runSum = true;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 6) {
+                runSum = false;
+            }
+
+            if (nums[i] == 7) {
+                if (!runSum) {
+                    runSum = true;
+                    continue;
+                }
+            }
+
+            if (runSum) {
+                sum = sum + nums[i];
+            }
+
+        }
+        AllOfMe.printMass(nums);
+
+        System.out.println(sum);
+
+    }
+}
+
