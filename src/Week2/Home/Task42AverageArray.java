03d07ed1b1886f6a203bf75ef8e0883cf7c54f4b
ilya.volkov <illiamvolkov@gmail.com>
2017-10-04 21:17:47 +0300

    add

---
+++ src/Week2/Home/Task42AverageArray.java
@@ -0,0 +1,30 @@
+package Week2.Home;
+
+import myClass.AllOfMe;
+
+public class Task42AverageArray {
+
+    public static void main(String[] args) {
+
+        int arNums[] = AllOfMe.createMassWithRandomNumber(15, -12, 14);
+
+        AllOfMe.printMass(arNums);
+
+        System.out.println("Среднее арифметическое массива = " + averageArray(arNums));
+
+    }
+
+    public static double averageArray(int[] arNums) {
+
+        int average = 0;
+
+        for (int i: arNums){
+            average += i;
+        }
+
+        return average/arNums.length;
+    }
+
+
+}
+
