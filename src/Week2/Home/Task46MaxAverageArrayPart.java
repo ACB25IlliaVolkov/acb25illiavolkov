d5e7cb879a2e38ec6f549a94baa1e2de455c95d7
ilya.volkov <illiamvolkov@gmail.com>
2017-10-05 21:15:01 +0300

    add

---
+++ src/Week2/Home/Task46MaxAverageArrayPart.java
@@ -0,0 +1,45 @@
+package Week2.Home;
+
+import myClass.AllOfMe;
+
+public class Task46MaxAverageArrayPart {
+
+    public static void main(String[] args) {
+
+        int[] arNums = AllOfMe.createMassWithRandomNumber(10, 1, 25);
+
+        int[] arNums1 = new int[5];
+        int[] arNums2 = new int[5];
+
+
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i=0; i<arNums.length; i++){
+
+            if (i<(arNums.length/2)) {
+
+                arNums1[i] = arNums[i];
+                sum1 += arNums[i];
+            }
+            else {
+
+                arNums2[i-5] = arNums[i];
+                sum2 += arNums[i];
+            }
+        }
+
+        AllOfMe.printMass(arNums);
+
+        if (sum1/arNums1.length < sum2/arNums2.length){
+
+            AllOfMe.printMass(arNums2);
+        }
+        else {
+
+            AllOfMe.printMass(arNums1);
+        }
+
+    }
+
+}
